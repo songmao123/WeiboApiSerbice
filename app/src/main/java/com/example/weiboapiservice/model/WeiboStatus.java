2294c4d6063b0ddf622969af6fec4cffed9a030b
SQSong <song.qingsong@moxiangroup.com>
2016-08-10 09:09:30 +0800

    modify WeiboStatus bean

--- app/src/main/java/com/example/weiboapiservice/model/WeiboStatus.java
+++ app/src/main/java/com/example/weiboapiservice/model/WeiboStatus.java
@@ -3,6 +3,8 @@ package com.example.weiboapiservice.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.List;
+
 /**
  * Created by sqsong on 16-8-6.
  */
@@ -21,6 +23,7 @@ public class WeiboStatus implements Parcelable {
     private String thumbnail_pic;
     private String bmiddle_pic;
     private String original_pic;
+    private List<WeiboPicture> pic_urls;
     private WeiboGeo geo;
     private WeiboUser user;
     private WeiboStatus retweeted_status;
@@ -29,6 +32,7 @@ public class WeiboStatus implements Parcelable {
     private int attitudes_count;
     private int mlevel;
     private String created_at;
+
 
     protected WeiboStatus(Parcel in) {
         id = in.readLong();
@@ -44,6 +48,7 @@ public class WeiboStatus implements Parcelable {
         thumbnail_pic = in.readString();
         bmiddle_pic = in.readString();
         original_pic = in.readString();
+        pic_urls = in.createTypedArrayList(WeiboPicture.CREATOR);
         geo = in.readParcelable(WeiboGeo.class.getClassLoader());
         user = in.readParcelable(WeiboUser.class.getClassLoader());
         retweeted_status = in.readParcelable(WeiboStatus.class.getClassLoader());
@@ -69,6 +74,7 @@ public class WeiboStatus implements Parcelable {
         dest.writeString(thumbnail_pic);
         dest.writeString(bmiddle_pic);
         dest.writeString(original_pic);
+        dest.writeTypedList(pic_urls);
         dest.writeParcelable(geo, flags);
         dest.writeParcelable(user, flags);
         dest.writeParcelable(retweeted_status, flags);
@@ -95,4 +101,180 @@ public class WeiboStatus implements Parcelable {
             return new WeiboStatus[size];
         }
     };
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getMid() {
+        return mid;
+    }
+
+    public void setMid(long mid) {
+        this.mid = mid;
+    }
+
+    public String getIdstr() {
+        return idstr;
+    }
+
+    public void setIdstr(String idstr) {
+        this.idstr = idstr;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public boolean isTruncated() {
+        return truncated;
+    }
+
+    public void setTruncated(boolean truncated) {
+        this.truncated = truncated;
+    }
+
+    public String getIn_reply_to_status_id() {
+        return in_reply_to_status_id;
+    }
+
+    public void setIn_reply_to_status_id(String in_reply_to_status_id) {
+        this.in_reply_to_status_id = in_reply_to_status_id;
+    }
+
+    public String getIn_reply_to_user_id() {
+        return in_reply_to_user_id;
+    }
+
+    public void setIn_reply_to_user_id(String in_reply_to_user_id) {
+        this.in_reply_to_user_id = in_reply_to_user_id;
+    }
+
+    public String getIn_reply_to_screen_name() {
+        return in_reply_to_screen_name;
+    }
+
+    public void setIn_reply_to_screen_name(String in_reply_to_screen_name) {
+        this.in_reply_to_screen_name = in_reply_to_screen_name;
+    }
+
+    public String getThumbnail_pic() {
+        return thumbnail_pic;
+    }
+
+    public void setThumbnail_pic(String thumbnail_pic) {
+        this.thumbnail_pic = thumbnail_pic;
+    }
+
+    public String getBmiddle_pic() {
+        return bmiddle_pic;
+    }
+
+    public void setBmiddle_pic(String bmiddle_pic) {
+        this.bmiddle_pic = bmiddle_pic;
+    }
+
+    public String getOriginal_pic() {
+        return original_pic;
+    }
+
+    public void setOriginal_pic(String original_pic) {
+        this.original_pic = original_pic;
+    }
+
+    public List<WeiboPicture> getPic_urls() {
+        return pic_urls;
+    }
+
+    public void setPic_urls(List<WeiboPicture> pic_urls) {
+        this.pic_urls = pic_urls;
+    }
+
+    public WeiboGeo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(WeiboGeo geo) {
+        this.geo = geo;
+    }
+
+    public WeiboUser getUser() {
+        return user;
+    }
+
+    public void setUser(WeiboUser user) {
+        this.user = user;
+    }
+
+    public WeiboStatus getRetweeted_status() {
+        return retweeted_status;
+    }
+
+    public void setRetweeted_status(WeiboStatus retweeted_status) {
+        this.retweeted_status = retweeted_status;
+    }
+
+    public int getReposts_count() {
+        return reposts_count;
+    }
+
+    public void setReposts_count(int reposts_count) {
+        this.reposts_count = reposts_count;
+    }
+
+    public int getComments_count() {
+        return comments_count;
+    }
+
+    public void setComments_count(int comments_count) {
+        this.comments_count = comments_count;
+    }
+
+    public int getAttitudes_count() {
+        return attitudes_count;
+    }
+
+    public void setAttitudes_count(int attitudes_count) {
+        this.attitudes_count = attitudes_count;
+    }
+
+    public int getMlevel() {
+        return mlevel;
+    }
+
+    public void setMlevel(int mlevel) {
+        this.mlevel = mlevel;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
 }
